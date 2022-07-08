6c421d0a4f53adaef2854a79c95ba0745d9e4360
Anatasia Chuvakova <anastasia@Anatasias-MacBook-Pro.local>
2022-07-07 17:58:46 -0700

    get fake shop cart data api

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { useQuery } from "react-query";
 //components
+import Item from "./item/item";
 import Drawer from "@material-ui/core/Drawer";
 import  LinearProgress from "@material-ui/core/LinearProgress";
 import Grid from "@material-ui/core/Grid";
@@ -29,10 +30,28 @@ const {data, isLoading, error} = useQuery<CartItemType[]>(
   'products', 
   getProducts
   );
-  return 
-    <div className="App">
-     Start
-    </div>
+  console.log(data);
+
+  const getTotalItems = () => null;
+
+  const handleAddToCart = (clickedItem: CartItemType) => null;
+
+  const handleRemoveFromCart = () => null;
+
+if(isLoading) return <LinearProgress />;
+if(error) return <div>Ooops, something went wrong</div>;
+
+  return (
+    <Wrapper>
+      <Grid container spacing={3}>
+        {data?.map(item => (
+          <Grid item key={item.id} xs={12} sm={4}>
+            <Item item={item} handleAddToCart={handleAddToCart} />
+            </Grid>
+        ))}
+      </Grid>
+    </Wrapper>
+  )
   
 }
 
